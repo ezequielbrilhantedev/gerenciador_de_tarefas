d0630823a74b08211ef9d1716d481930f0b9de11
Ezequiel Brilhante <ezequielrb7@gmail.com>
2020-05-05 00:53:34 -0300

    Primeiro commit, iniciando o projeto

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def listar_tarefas(request):
+    nome_tarefa = 'Assistir a Semana Python Django da TreinaWeb'
+    return render(request, 'tarefas/listar_tarefas.html', {'nome_tarefa': nome_tarefa})
